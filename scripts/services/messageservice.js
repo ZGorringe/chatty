ac1a47baa0321fd90ccfdc2140413af85396991c
ZGorringe <zacgorringe@gmail.com>
2014-10-16 10:31:29 -0600

    Updated Service.js

--- scripts/services/messageservice.js
+++ scripts/services/messageservice.js
@@ -3,5 +3,25 @@
 angular.module('chattyApp')
   .service('MessageService', function MessageService() {
     return {
+
+    	getMessage: function(){
+    		return $http({
+    			method: 'GET',
+    			url:'http://localhost:8500'
+    		}).then(function(data) {
+    			return data.data;
+    		})
+    	},
+
+    	postMessage: function(){
+    		return $http({
+    			method: 'PUT',
+    			url: 'http://localhost:8500',
+    			data: {text: newChat}
+    		}).then(function(data) {
+    			return data.data;
+    		})
+    	}
     }
+
   });
